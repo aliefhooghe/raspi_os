c48ff6b507955f48468a4d70e20e3f0bbd856d83
Arthur Liefhooghe <aliefhooghe@gmail.com>
2025-01-13 01:10:23 +0100

    Add welcom message

--- src/kernel.c
+++ src/kernel.c
@@ -41,24 +41,37 @@
 
 #define MMIO_BASE 0x20000000
 
+static const char *welcome_message =
+"  ________        __       ___________        __       _____  ___           ______      ________\n"
+" /\"       )      /\"\"\\     (\"     _   \")      /\"\"\\     (\\\"   \\|\"  \\         /    \" \\    /\"       \n"
+"(:   \\___/      /    \\     )__/  \\\\__/      /    \\    |.\\\\   \\    |       // ____  \\  (:   \\___/\n"
+" \\___  \\       /' /\\  \\       \\\\_ /        /' /\\  \\   |: \\.   \\\\  |      /  /    ) :)  \\___  \\\n"
+"  __/  \\\\     //  __'  \\      |.  |       //  __'  \\  |.  \\    \\. |     (: (____/ //    __/  \\\\\n"
+" /\" \\   :)   /   /  \\\\  \\     \\:  |      /   /  \\\\  \\ |    \\    \\ |      \\        /    /\" \\   :)\n"
+"(_______/   (___/    \\___)     \\__|     (___/    \\___) \\___|\\____\\)       \\\"_____/    (_______/\n"
+;
+
+
+
+
 // Memory-Mapped I/O output
 static inline void mmio_write(uint32_t reg, uint32_t data)
 {
-	*(volatile uint32_t*)(MMIO_BASE + reg) = data;
+    *(volatile uint32_t*)(MMIO_BASE + reg) = data;
 }
 
 // Memory-Mapped I/O input
 static inline uint32_t mmio_read(uint32_t reg)
 {
-	return *(volatile uint32_t*)(MMIO_BASE + reg);
+    return *(volatile uint32_t*)(MMIO_BASE + reg);
 }
 
 // Loop <delay> times in a way that the compiler won't optimize away
 static inline void delay(int32_t count)
 {
-	asm volatile("__delay_%=: subs %[count], %[count], #1\n"
-		   " bne __delay_%=\n"
-		 : "=r"(count): [count]"0"(count) : "cc");
+    asm volatile("__delay_%=: subs %[count], %[count], #1\n"
+           " bne __delay_%=\n"
+         : "=r"(count): [count]"0"(count) : "cc");
 }
 
 
@@ -69,50 +82,50 @@ volatile unsigned int  __attribute__((aligned(16))) mbox[9] = {
 
 void uart_init()
 {
-	// Disable UART0.
-	mmio_write(UART0_CR, 0x00000000);
-	// Setup the GPIO pin 14 && 15.
+    // Disable UART0.
+    mmio_write(UART0_CR, 0x00000000);
+    // Setup the GPIO pin 14 && 15.
 
-	// Disable pull up/down for all GPIO pins & delay for 150 cycles.
-	mmio_write(GPPUD, 0x00000000);
-	delay(150);
+    // Disable pull up/down for all GPIO pins & delay for 150 cycles.
+    mmio_write(GPPUD, 0x00000000);
+    delay(150);
 
-	// Disable pull up/down for pin 14,15 & delay for 150 cycles.
-	mmio_write(GPPUDCLK0, (1 << 14) | (1 << 15));
-	delay(150);
+    // Disable pull up/down for pin 14,15 & delay for 150 cycles.
+    mmio_write(GPPUDCLK0, (1 << 14) | (1 << 15));
+    delay(150);
 
-	// Write 0 to GPPUDCLK0 to make it take effect.
-	mmio_write(GPPUDCLK0, 0x00000000);
+    // Write 0 to GPPUDCLK0 to make it take effect.
+    mmio_write(GPPUDCLK0, 0x00000000);
 
-	// Clear pending interrupts.
-	mmio_write(UART0_ICR, 0x7FF);
+    // Clear pending interrupts.
+    mmio_write(UART0_ICR, 0x7FF);
 
-	// Set integer & fractional part of baud rate.
-	// Divider = UART_CLOCK/(16 * Baud)
-	// Fraction part register = (Fractional part * 64) + 0.5
-	// Baud = 115200.
+    // Set integer & fractional part of baud rate.
+    // Divider = UART_CLOCK/(16 * Baud)
+    // Fraction part register = (Fractional part * 64) + 0.5
+    // Baud = 115200.
 
-	// Divider = 3000000 / (16 * 115200) = 1.627 = ~1.
-	mmio_write(UART0_IBRD, 1);
-	// Fractional part register = (.627 * 64) + 0.5 = 40.6 = ~40.
-	mmio_write(UART0_FBRD, 40);
+    // Divider = 3000000 / (16 * 115200) = 1.627 = ~1.
+    mmio_write(UART0_IBRD, 1);
+    // Fractional part register = (.627 * 64) + 0.5 = 40.6 = ~40.
+    mmio_write(UART0_FBRD, 40);
 
-	// Enable FIFO & 8 bit data transmission (1 stop bit, no parity).
-	mmio_write(UART0_LCRH, (1 << 4) | (1 << 5) | (1 << 6));
+    // Enable FIFO & 8 bit data transmission (1 stop bit, no parity).
+    mmio_write(UART0_LCRH, (1 << 4) | (1 << 5) | (1 << 6));
 
-	// Mask all interrupts.
-	mmio_write(UART0_IMSC, (1 << 1) | (1 << 4) | (1 << 5) | (1 << 6) |
-	                       (1 << 7) | (1 << 8) | (1 << 9) | (1 << 10));
+    // Mask all interrupts.
+    mmio_write(UART0_IMSC, (1 << 1) | (1 << 4) | (1 << 5) | (1 << 6) |
+                           (1 << 7) | (1 << 8) | (1 << 9) | (1 << 10));
 
-	// Enable UART0, receive & transfer part of UART.
-	mmio_write(UART0_CR, (1 << 0) | (1 << 8) | (1 << 9));
+    // Enable UART0, receive & transfer part of UART.
+    mmio_write(UART0_CR, (1 << 0) | (1 << 8) | (1 << 9));
 }
 
 void uart_putc(unsigned char c)
 {
-	// Wait for UART to become ready to transmit.
-	while ( mmio_read(UART0_FR) & (1 << 5) ) { }
-	mmio_write(UART0_DR, c);
+    // Wait for UART to become ready to transmit.
+    while ( mmio_read(UART0_FR) & (1 << 5) ) { }
+    mmio_write(UART0_DR, c);
 }
 
 unsigned char uart_getc()
@@ -124,21 +137,42 @@ unsigned char uart_getc()
 
 void uart_puts(const char* str)
 {
-	for (size_t i = 0; str[i] != '\0'; i ++)
-		uart_putc((unsigned char)str[i]);
+    for (size_t i = 0; str[i] != '\0'; i ++)
+        uart_putc((unsigned char)str[i]);
 }
 
+void uart_put_int(uint32_t x)
+{
+    char result[12] = "";
+    int index = 11;
+
+    while (x > 0) {
+        result[--index] = '0' + (x % 10);
+        x /= 10;
+    }
+
+    uart_puts(result + index);
+}
 
 // arguments for AArch32
 void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags)
 {
-	// initialize UART for Raspi0
-	uart_init();
-	uart_puts("Hello, kernel World!\r\n");
+    // initialize UART for Raspi0
+    uart_init();
 
-	while (1) {
-		const char car = uart_getc();
-		uart_putc(car);
-		uart_putc(car);
-	}
+    // print a welcome message ;)
+    uart_puts(welcome_message);
+
+    char car = '\r';
+    do {
+        if (car == '\r')
+        {
+            uart_puts("\nsatan ~ ");
+        }
+        else {
+            uart_putc(car == '\r' ? '\n' : car);
+        }
+
+        car = uart_getc();
+    } while (1);
 }
