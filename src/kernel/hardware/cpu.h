93d1890c3245adbf254fbd23402ee953b8229a00
Arthur Liefhooghe <aliefhooghe@gmail.com>
2025-01-19 00:04:52 +0100

    Enable irq/get execution  mode

--- src/kernel/hardware/cpu.h
+++ src/kernel/hardware/cpu.h
@@ -5,11 +5,36 @@
 
 extern void cpu_delay(uint32_t cycle_count);
 
+
+
+
+
+/**
+ *  # APSR – Application Program Status Register
+ *
+ *  # CPSR – Current Program Status Register
+ *
+ *   +========+========+=========================================+
+ *   | Bit(s) | name   | info                                    |
+ *   +========+========+=========================================+
+ *   | 31     | N      |                                         |
+ *   | 30     | Z      |                                         |
+ *   | 29     | C      |                                         |
+ *   | 28     | V      |                                         |
+ *   | 27-8   | unused |                                         |
+ *   | 7      | I      | Disables IRQ interrupts when it is set. |
+ *   | 6      | F      | Disables FIQ interrupts when it is set. |
+ *   | 5      | T      | Thumb mode                              |
+ *   | 4-0    | M      | cpu mode (see bellow)                   |
+ *   +--------+--------+-----------------------------------------+
+ *
+ *  # SPSR – Saved Program Status Register
+ */
+
 /**
  * Notes:
  * - The presence of particular processor modes and states depends on whether the processor implements the relevant architecture extension
  */
-
 //      Mode                Encoding       Function                                                                  SecurityState   PrivilegeLevel
 #define CPU_MODE_USER        0X10u      // Unprivileged mode in which most applications run                          Both            PL0
 #define CPU_MODE_FIQ         0X11u      // Entered on an FIQ interrupt exception                                     Both            PL1
@@ -21,14 +46,20 @@ extern void cpu_delay(uint32_t cycle_count);
 #define CPU_MODE_UNDEF       0X1Bu      // Entered when an undefined instruction executed                            Both            PL1
 #define CPU_MODE_SYSTEM      0X1Fu      // Privileged mode, sharing the register view with User mode                 Both            PL1
 
+// a formatter: registre acceccibles par mode.
+// 10000 	User 	PC, R14 to R0, CPSR
+// 10001 	FIQ 	PC, R14_fiq to R8_fiq, R7 to R0, CPSR, SPSR_fiq
+// 10010 	IRQ 	PC, R14_irq, R13_irq, R12 to R0, CPSR, SPSR_irq
+// 10011 	Supervisor 	PC, R14_svc, R13_svc, R12 to R0, CPSR, SPSR_svc
+// 10111 	Abort 	PC, R14_abt, R13_abt, R12 to R0, CPSR, SPSR_abt
+// 11011 	Undefined 	PC, R14_und, R13_und, R12 to R0, CPSR, SPSR_und
+// 11111 	System 	PC, R14 to R0, CPSR
+
+
+
+
+void cpu_irq_enable(void);
 uint8_t cpu_get_execution_mode(void);
-
-/**
- *  APSR – Application Program Status Register
- *  CPSR – Current Program Status Register
- *  SPSR – Saved Program Status Register
- */
-
 
 
 #endif
