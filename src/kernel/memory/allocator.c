7965c86c83afeba9fe3df60d0c19a573c63e459f
Arthur Liefhooghe <aliefhooghe@gmail.com>
2025-02-01 10:11:51 +0100

    basic allocator

--- src/kernel/memory/allocator.c
+++ src/kernel/memory/allocator.c
@@ -1,18 +1,32 @@
 
 #include "allocator.h"
 
-#include "lib/str.h"
+#include <stdint.h>
 
-void memory_allocator_init(memory_allocator_t *allocator)
+void memory_allocator_init(
+    memory_allocator_t *allocator,
+    uint32_t base,
+    uint32_t size)
 {
-    _memset(&allocator, 0, sizeof(memory_allocator_t));
+    allocator->limit = base;
+    allocator->base = base;
+    allocator->size = size;
 }
 
 void *memory_allocator_alloc(memory_allocator_t *allocator, size_t size)
 {
-    (void)allocator,
-    (void)size;
-    return NULL;
+    const uint32_t max_limit = allocator->base + allocator->size;
+    const uint32_t new_limit = allocator->limit + size;
+    if (new_limit > max_limit)
+    {
+        return NULL;
+    }
+    else
+    {
+        const uint32_t result = allocator->limit;
+        allocator->limit = new_limit;
+        return (void*)result;
+    }
 }
 
 void memory_allocator_free(memory_allocator_t *allocator, void *chunk)
