28e944d7ad87aba6c7502635f68f2d24c33ad9db
Arthur Liefhooghe <aliefhooghe@gmail.com>
2025-07-27 20:51:01 +0200

    More vfs tests in user mode

--- src/kernel/usermode/usermode.c
+++ src/kernel/usermode/usermode.c
@@ -25,6 +25,23 @@ static void test_fork(int32_t pid, FILE *stdout)
             fprintf(stdout, "[%u] child process is working iteration=%u\n", cpid, i);
         }
 
+        // VFS tests
+        int fd = usr_syscall_open("/tty", 0u, 0u);
+        FILE file = { .fd = fd, .write_buffer_cursor = 0u };
+        fprintf(&file, "[%u] WRITE TO TTY\n", cpid);
+        fflush(&file);
+
+        //
+        int fd2 = usr_syscall_open("/toto/tata.txt", 0u, 0u);
+        if (fd2 == -1)
+        {
+            fprintf(stdout, "[%u] could not open toto/tata.txt\n", cpid);
+        }
+        else
+        {
+            fprintf(stdout, "[%u] HOOORIBLE", cpid);
+        }
+
         fprintf(stdout, "[%u] exit child process.\n", cpid);
         usr_syscall_exit(42u);
     }
