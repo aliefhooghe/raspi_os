58a33a026e79381f8aeab8f5a49bb643cb881d5f
Arthur Liefhooghe <aliefhooghe@gmail.com>
2025-08-02 15:20:28 +0200

    Implement the division

--- src/kernel/lib/eabi_rt.c
+++ src/kernel/lib/eabi_rt.c
@@ -1,10 +1,15 @@
 
+#include <stdint.h>
 #include "lib/str.h"
 
-/**
- *  compiler runtime library
- */
+// 
+//  Compiler Runtime Library
+// 
 
+
+//
+// Memory management
+// 
 void __aeabi_memcpy8(void *dest, const void *src, size_t n)
 {
     _memcpy(dest, src, n);
@@ -35,17 +40,17 @@ void __aeabi_memcpy(void *dest, const void *src, size_t n)
 //     _memmove(dest, src, n);
 // }
 
-void __aeabi_memset8(void *dest, size_t n, int c)
+void __aeabi_memset8(void *dest, size_t n, int32_t c)
 {
     _memset(dest, c, n);
 }
 
-void __aeabi_memset4(void *dest, size_t n, int c)
+void __aeabi_memset4(void *dest, size_t n, int32_t c)
 {
     _memset(dest, c, n);
 }
 
-void __aeabi_memset(void *dest, size_t n, int c)
+void __aeabi_memset(void *dest, size_t n, int32_t c)
 {
     _memset(dest, c, n);
 }
@@ -64,3 +69,41 @@ void __aeabi_memclr(void *dest, size_t n)
 {
     __aeabi_memset(dest, n, 0);
 }
+
+//
+// Math
+//
+
+// signed division
+// int32_t __aeabi_idiv(int32_t numerator, int32_t denominator)
+// {
+//     if (denominator == 0)
+//         return 0; // TODO: trap the divide by zero
+
+    
+// }
+
+// int32_t __aeabi_idivmod(int32_t numerator, int32_t denominator)
+// {
+    
+// }
+
+// unsigned division
+uint32_t __aeabi_uidiv(uint32_t numerator, uint32_t denominator)
+{
+    if (denominator == 0)
+        return 0; // TODO: trap the divide by zero
+
+    uint32_t quo = 0;
+    while (numerator >= denominator) {
+        numerator -= denominator;
+        quo++;
+    }
+
+    return quo;
+}
+
+// uint32_t __aeabi_uidivmod(uint32_t numerator, uint32_t denominator)
+// {
+    
+// }
