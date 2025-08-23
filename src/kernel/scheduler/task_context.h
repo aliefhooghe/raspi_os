d9b16c155c2bbca9918652c0b508aa9eacb7fd92
Arthur Liefhooghe <aliefhooghe@gmail.com>
2025-08-23 17:58:12 +0200

    fix: save usr lr in addition to svc lr

--- src/kernel/scheduler/task_context.h
+++ src/kernel/scheduler/task_context.h
@@ -5,14 +5,16 @@
 
 // 
 // Cpu registers which must be saved and restored when switching context
-// 
+//
+// Layout is important. See interupt.S / scheduler.S
 typedef struct {
-    uint32_t r0;    // syscall return status
+    uint32_t r0;      // syscall return status
 
-    uint32_t sp;    // stack pointer
-    uint32_t spsr;  // saved status register
+    uint32_t lr_usr;  // user return address register
+    uint32_t sp;      // stack pointer
+    uint32_t spsr;    // saved status register
 
-    uint32_t r1;    // user registers
+    uint32_t r1;      // user registers
     uint32_t r2;
     uint32_t r3;
     uint32_t r4;
@@ -25,7 +27,13 @@ typedef struct {
     uint32_t r11;
     uint32_t r12;
 
-    uint32_t lr;    // return address register. Actually lr_svc
+    uint32_t lr_svc;    // svc return address register: pc_usr + 4
 } task_context_t;
+
+// if this change fix is needed in svc_handler (interupt.S)
+_Static_assert(
+    sizeof(task_context_t) == 68u,
+    "sizeof(task_context_t) as changed"
+);
 
 #endif
