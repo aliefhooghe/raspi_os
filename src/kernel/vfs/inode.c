90302d12a9e8cb381ff09aef93490116e4057c5c
Arthur Liefhooghe <aliefhooghe@gmail.com>
2026-02-01 16:38:07 +0100

    Add forgotten inode.c file

---
+++ src/kernel/vfs/inode.c
@@ -0,0 +1,26 @@
+
+#include "kernel.h"
+#include "memory/memory_allocator.h"
+#include "inode.h"
+
+
+
+file_t *default_file_open(inode_t *inode)
+{
+    KERNEL_ASSERT(inode != NULL);
+    file_t *file = memory_calloc(sizeof(file_t));
+    KERNEL_ASSERT(file != NULL);
+    file->inode = inode;
+    file->pos = 0u;
+    file->private = NULL;
+    return file;
+}
+
+int default_file_release(inode_t *inode, file_t *file)
+{
+    KERNEL_ASSERT(inode != NULL);
+    KERNEL_ASSERT(file != NULL);
+    KERNEL_ASSERT(file->fd_count == 0u);
+    memory_free(file);
+    return 0;
+}
